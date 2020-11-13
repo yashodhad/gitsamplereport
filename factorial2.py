4480f0c6cdf8287c0ad3160182ceb54f6efa99f1
yashu <yashuputta15@gmail.com>
2020-11-13 13:11:20 +0530

    commit clone file

---
+++ factorial2.py
@@ -0,0 +1,18 @@
+#5=1*2*3*4*5= 120
+# Prints the status of the git repository
+# Initialize the repo
+#git difference rt
+#new folder
+#after cloning we changed this comment
+fact=1
+num=5
+
+if num<0:
+    print("Factorial does not exist for negative number")
+if num==0:
+    print("the factorial of 0 is 1")
+else:
+    for i in range(1, num + 1):
+        fact=fact *i
+    print("the fact of ",num, "is",fact)
+
